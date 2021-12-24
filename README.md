648d417ec11f99f4d4fec2371890310b60de0a6f
Kyrylo Danylov <kyrylo.danylov@yalantis.net>
2021-12-24 03:08:49 +0200

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+To get template just run 
+
+source < (curl -s https://raw.githubusercontent.com/Styrmist/YalantisTemplate/master/get_template.sh)
--- get_template.sh
+++ get_template.sh
@@ -1,5 +1,3 @@
 dir="/Users/$(whoami)/Library/Developer/Xcode/Templates/Custom Templates"
 mkdir -p "$dir"
-
 curl -s https://codeload.github.com/Styrmist/YalantisTemplate/tar.gz/master | tar -zvx --strip=1 -C "$dir"
-
