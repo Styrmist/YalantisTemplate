eaca7a978b89b6433f4580f551e869b50875dd75
Kyrylo Danylov <kyrylo.danylov@yalantis.net>
2021-12-24 03:04:58 +0200

    add get script

---
+++ get_template.sh
@@ -0,0 +1,5 @@
+dir="/Users/$(whoami)/Library/Developer/Xcode/Templates/Custom Templates"
+mkdir -p "$dir"
+
+curl -s https://codeload.github.com/Styrmist/YalantisTemplate/tar.gz/master | tar -zvx --strip=1 -C "$dir"
+
